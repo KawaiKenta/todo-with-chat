5c9c8d86ac793a03849c329e21cb3861ca98a11f
asahi1199 <asahi.yoshida.1130@gmail.com>
2023-11-13 05:12:36 +0900

    [add] prettier, react-router-dom の導入

--- frontend/todo-react/src/App.tsx
+++ frontend/todo-react/src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import UserPage from './pages/UserPage';
+import NotFound from './components/NotFound';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div>Header</div>
+      <Container>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/user/:userId" element={<UserPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+      <div>Footer</div>
+    </Container>
   );
-}
+};
 
 export default App;
