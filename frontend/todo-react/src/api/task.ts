df76f0ee4c169af2230bf90a05098ea00edbeffb
asahi1199 <asahi.yoshida.1130@gmail.com>
2023-11-16 01:56:27 +0900

    #12 [mod] 不要なコメントを削除

--- frontend/todo-react/src/api/task.ts
+++ frontend/todo-react/src/api/task.ts
@@ -1,39 +1,8 @@
-import React from 'react';
-import axios, { AxiosResponse, AxiosError, AxiosPromise } from 'axios';
+import axios from 'axios';
 import { TaskParams } from '../types/task';
 import { API_BASE_URL } from '../constants';
 
-// type FetchTasksByUser = () => AxiosPromise<TaskParams[]>;
-
-// あるユーザーのすべてのタスクを取得するメソッド
-// export const useFetchTasksByUser =  (
-//   id: number
-// ) => {
-//   const fetchTasksByUser: FetchTasksByUser = async () => {
-//     try {
-//       const res: AxiosResponse<TaskParams[]> = await axios.get(
-//         `${API_BASE_URL}/user/${id}/tasks`
-//       );
-//       return res;
-//     } catch (error) {
-//       console.error('Error while fetching samples: ', error);
-//       throw error;
-//     }
-//   };
-//   return {fetchTasksByUser}
-// };
-
-// axios
-//   .get(`${API_BASE_URL}/user/${id}/tasks`)
-//   .then((res: AxiosResponse<TaskParams[]>) => {
-//     const { data, status } = res;
-//     // console.log(status)
-//     return data;
-//   })
-//   .catch((e: AxiosError<{ error: string }>) => {
-//     console.log('Error while fetching samples: ', e);
-//   });
-
+// 1ユーザーに紐づくすべてのタスクを取得するメソッド
 export const fetchTasksByUser = async (id: number): Promise<TaskParams[]> => {
   try {
     const response = await axios.get(`${API_BASE_URL}/user/${id}/tasks`);
