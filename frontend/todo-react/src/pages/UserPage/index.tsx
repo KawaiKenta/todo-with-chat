b0692899de7ad94b938bd168534e60323c916300
asahi1199 <asahi.yoshida.1130@gmail.com>
2023-11-16 01:30:16 +0900

    tmp

--- frontend/todo-react/src/pages/UserPage/index.tsx
+++ frontend/todo-react/src/pages/UserPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import NotFound from '../../components/NotFound';
 import { Box, Fab } from '@mui/material';
@@ -9,95 +9,125 @@ import { TaskParams } from '../../types/task';
 import { TaskList } from '../../components/TaskList';
 import { Modal } from '../../components/Modal';
 import { TaskForm } from '../../components/TaskForm';
+// import { useFetchTasksByUser } from '../../api/task';
+import { AxiosResponse } from 'axios';
+import { fetchTasksByUser } from '../../api/task';
 
 // サンプルデータ (API からデータを取得する処理を追加する)
-const sampleTaskParams: TaskParams[] = [
-  {
-    id: 1,
-    user_id: 1,
-    title: '月次報告書の作成',
-    content: '先月の業績や課題をまとめて報告書を作成',
-    due_date: '2023-12-31',
-    priority: '中',
-    status: '未着手',
-    last_modified_by: 'user',
-  },
-  {
-    id: 2,
-    user_id: 1,
-    title: 'クライアントミーティングの準備',
-    content: '次回のクライアントミーティングに向けて資料の準備',
-    due_date: '2024-01-05',
-    priority: '高',
-    status: '未着手',
-    last_modified_by: 'user',
-  },
-  {
-    id: 3,
-    user_id: 1,
-    title: 'プロジェクト進捗の調査',
-    content: 'プロジェクトの進捗状況を確認し、課題点を洗い出す',
-    due_date: null,
-    priority: '中',
-    status: '完了',
-    last_modified_by: 'AI',
-  },
-  {
-    id: 4,
-    user_id: 1,
-    title: '新プロジェクトのキックオフミーティング',
-    content: '新しいプロジェクトの計画や目標を確認するためのミーティングに参加',
-    due_date: '2023-12-31',
-    priority: '中',
-    status: '着手中',
-    last_modified_by: 'user',
-  },
-  {
-    id: 5,
-    user_id: 1,
-    title: 'クライアント契約交渉',
-    content: '新規クライアントとの契約交渉を行い、条件や提案を調整',
-    due_date: null,
-    priority: '低',
-    status: '完了',
-    last_modified_by: 'AI',
-  },
-  {
-    id: 6,
-    user_id: 1,
-    title: 'クライアントからの質問回答',
-    content: 'クライアントからの質問に対する回答をまとめる',
-    due_date: '2023-12-18',
-    priority: '中',
-    status: '削除済み',
-    last_modified_by: 'user',
-  },
-];
+// const sampleTaskParams: TaskParams[] = [
+//   {
+//     id: 1,
+//     user_id: 1,
+//     title: '月次報告書の作成',
+//     content: '先月の業績や課題をまとめて報告書を作成',
+//     due_date: '2023-12-31',
+//     priority: '中',
+//     status: '未着手',
+//     last_modified_by: 'user',
+//   },
+//   {
+//     id: 2,
+//     user_id: 1,
+//     title: 'クライアントミーティングの準備',
+//     content: '次回のクライアントミーティングに向けて資料の準備',
+//     due_date: '2024-01-05',
+//     priority: '高',
+//     status: '未着手',
+//     last_modified_by: 'user',
+//   },
+//   {
+//     id: 3,
+//     user_id: 1,
+//     title: 'プロジェクト進捗の調査',
+//     content: 'プロジェクトの進捗状況を確認し、課題点を洗い出す',
+//     due_date: null,
+//     priority: '中',
+//     status: '完了',
+//     last_modified_by: 'AI',
+//   },
+//   {
+//     id: 4,
+//     user_id: 1,
+//     title: '新プロジェクトのキックオフミーティング',
+//     content: '新しいプロジェクトの計画や目標を確認するためのミーティングに参加',
+//     due_date: '2023-12-31',
+//     priority: '中',
+//     status: '着手中',
+//     last_modified_by: 'user',
+//   },
+//   {
+//     id: 5,
+//     user_id: 1,
+//     title: 'クライアント契約交渉',
+//     content: '新規クライアントとの契約交渉を行い、条件や提案を調整',
+//     due_date: null,
+//     priority: '低',
+//     status: '完了',
+//     last_modified_by: 'AI',
+//   },
+//   {
+//     id: 6,
+//     user_id: 1,
+//     title: 'クライアントからの質問回答',
+//     content: 'クライアントからの質問に対する回答をまとめる',
+//     due_date: '2023-12-18',
+//     priority: '中',
+//     status: '削除済み',
+//     last_modified_by: 'user',
+//   },
+// ];
 
 // const currentUserParams: UserParams =
 
-const RenderedComponent = (userId: string) => {
-  // userId が数値であるかを判定
-  // aaa や 123bb などがはじかれる
-  if (!isNaN(Number(userId))) {
-    return (
-      <>
-        {/* <h1>ユーザーページ</h1> */}
-        {/* <h2>ユーザーID: {userId}</h2> */}
-        {/* userId を数値として使用する場合 */}
-        {/* parseInt(userId, 10) */}
-        <TaskList tasks={sampleTaskParams} />
-      </>
-    );
-  } else {
-    // データを正しく取得できない場合のエラーを入れる
-    return <NotFound />;
-  }
-};
-
 const UserPage: FC = () => {
-  const [currentUser] = useAtom(currentUserAtom);
+  // URLからユーザIDを取得
+  const userIdParam = useParams<{ userId: string }>().userId;
+  // console.log(userIdParam);
+
+  // console.log(userId);
+  // ユーザIDを数値に変換
+  const userId: number = !isNaN(Number(userIdParam)) ? Number(userIdParam) : 0;
+  // console.log(userId);
+
+  // const [currentUser] = useAtom(currentUserAtom);
   const [newTaskModalOpen, setNewTaskModalOpen] = useState<boolean>(false);
+  const [fetchedTasks, setFetchedTasks] = useState<TaskParams[]>([]);
+  // const { fetchTasksByUser } = userId
+  //   ? // eslint-disable-next-line react-hooks/rules-of-hooks
+  //     useFetchTasksByUser(Number(userId))
+  //   : // eslint-disable-next-line react-hooks/rules-of-hooks
+  //     useFetchTasksByUser(0);
+
+  // API からユーザーに紐づいたタスクを読み取り
+  useEffect(() => {
+    (async () => {
+      const fetchedData = await fetchTasksByUser(userId);
+      setFetchedTasks(fetchedData);
+    })();
+  }, []);
+
+  // const fetchedTasks = userId ? useFetchTasksByUser(Number(userId)) : undefined;
+  console.log(fetchedTasks);
+
+  const RenderedComponent = () => {
+    <>{<TaskList tasks={fetchedTasks} />}</>;
+    // userId が数値であるかを判定
+    // aaa や 123bb などがはじかれる
+    // if (!isNaN(Number(userId))) {
+    //   return (
+    //     <>
+    //       {/* <h1>ユーザーページ</h1> */}
+    //       {/* <h2>ユーザーID: {userId}</h2> */}
+    //       {/* userId を数値として使用する場合 */}
+    //       {/* parseInt(userId, 10) */}
+    //       {fetchedTasks && <TaskList tasks={fetchedTasks} />}
+    //     </>
+    //   );
+    // } else {
+    //   // データを正しく取得できない場合のエラーを入れる
+    //   return <NotFound />;
+    // }
+  };
 
   // Modal の onClose 関数
   const handleNewTaskModalClose = (
@@ -114,19 +144,16 @@ const UserPage: FC = () => {
     console.log('submitted');
     setNewTaskModalOpen(false);
   };
-
-  console.log(currentUser);
-  // URLからユーザIDを取得
-  const { userId } = useParams<{ userId: string }>();
-  // console.log(userId);
-
   return (
     <Box sx={{ width: '90%', margin: '10px auto' }}>
       <>
         <Box component="h1" color="#000000df" fontSize="28px">
           タスク一覧
         </Box>
-        {userId && RenderedComponent(userId)}
+        {/* {RenderedComponent()}
+         */}
+        <TaskList tasks={fetchedTasks} />
+        {/* {fetchedTasks && <TaskList tasks={fetchedTasks} />} */}
         {/* 新規タスクボタン */}
         <Fab
           sx={{
